f206a444fb34e94bd8251b703bccf2d3d4d0079f
Leandro Almeida <lcavalcanti.almeida@gmail.com>
2022-03-22 21:00:36 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+(async () => {
+  const logs = [];
+
+  const config = {
+    headless: false,
+    defaultViewport: null,
+    executablePath:
+      '/usr/bin/google-chrome',
+  };
+
+  const browser = await puppeteer.launch(config);
+
+  const page = await browser.newPage();
+
+  page.on('console', (msg) => {
+    console.log('LOG:', msg.text());
+    logs.push(msg.text());
+  });
+
+  await page.goto(`file://${__dirname}/index.html`);
+
+  const videoDuration = 600 * 1000; // 10 minutes (600 sec)
+
+  await page.click('video');
+
+  await wait(videoDuration);
+
+  await browser.close();
+
+  const time = new Date().getTime();
+
+  fs.writeFileSync(`logs-${time}.txt`, logs.join('\n'));
+})();
